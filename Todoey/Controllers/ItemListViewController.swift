5d8e9540bdecd4730a086639fc9e9272fff0e751
Ken Loh <ken@proappconcept.com>
2019-06-17 19:21:54 +0800

    Complete UI, Data Model & Setting.

---
+++ Todoey/Controllers/ItemListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ItemListViewController.swift
+//  Todoey
+//
+//  Created by Kenny Loh on 17/6/19.
+//  Copyright © 2019 ProApp Concept Pte Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ItemListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
